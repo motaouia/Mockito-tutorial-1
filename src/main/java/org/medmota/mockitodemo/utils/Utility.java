e5f011c65316e5868b8186d718efe291e5eb0eab
motaouia <med.motaouia@gmail.com>
2022-08-30 19:46:44 +0200

    mock-final-class-and-static-method

---
+++ src/main/java/org/medmota/mockitodemo/utils/Utility.java
@@ -0,0 +1,8 @@
+package org.medmota.mockitodemo.utils;
+
+public class Utility {
+
+	public static String getDatabaseConnection(String url) {
+		return "http:///production/" + url;
+	}
+}
---
+++ src/test/java/org/medmota/mockitodemo/utils/UtilityTest.java
@@ -0,0 +1,28 @@
+package org.medmota.mockitodemo.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+class UtilityTest {
+
+	@Test
+	void shouldMockStaticMethod() {
+		MockedStatic<FinalClass> mockStatic = Mockito.mockStatic(FinalClass.class);
+		try (MockedStatic<Utility> mockedStatic = Mockito.mockStatic(Utility.class)) {
+
+			mockedStatic.when(() -> Utility.getDatabaseConnection(Mockito.eq("test"))).thenReturn("testing");
+			mockedStatic.when(() -> Utility.getDatabaseConnection(Mockito.eq("prod"))).thenReturn("production");
+
+			String result1 = Utility.getDatabaseConnection("test");
+
+			assertEquals("testing", result1);
+			String result2 = Utility.getDatabaseConnection("prod");
+			assertEquals("production", result2);
+
+		}
+
+	}
+}
